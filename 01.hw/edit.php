f9134d0a69512223c17db4c2396595fbae7ffdf1
Bobby Kolev <bobbykolev@abv.bg>
2013-09-25 13:03:56 +0300

    expenses improvements

--- 01.hw/edit.php
+++ 01.hw/edit.php
@@ -11,13 +11,13 @@ include 'includes/header.php';
 	
     if(mb_strlen($postData[1])<4){
         echo '<p class="lead">The product name is too short.</p>';
-		header("refresh:3;url=index.php");
+		header("refresh:2;url=index.php");
         $postData[4]=true;
     }
 	
     if(!is_numeric($postData[2]) || floatval($postData[2]) > 10000000 || floatval($postData[2]) < 0.01){
         echo '<p class="lead">Invalid expense.</p>';
-		header("refresh:3;url=index.php");
+		header("refresh:2;url=index.php");
         $postData[4]=true;
     }
     
@@ -32,7 +32,7 @@ include 'includes/header.php';
     	file_put_contents($data, $fileContent);
         
             echo '<p class="lead">"'.$postData[1].'" successfuly edited.</p>';
-			header("refresh:2;url=index.php");        
+			header("refresh:1;url=index.php");        
     } ?>
 </div>
 <?php
